3effbc09bd841bda833e55c114d95e627cac4563
Craig Patik <cpatik@gmail.com>
2013-04-13 14:10:59 -0400

    Reverted structural changes, removed uglify for now

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,43 +3,28 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-                mangle: false, // Don't change variable and function names
-                report: 'min'  // Print size savings to the command line
-            },
-            my_target: {
-                files: {
-                    'js/excelsior/excelsior.min.js': ['src/js/excelsior/excelsior.js'],
-                    'js/excelsior/off-canvas.min.js': ['src/js/excelsior/off-canvas.js'],
-                    'js/excelsior/respCharts.min.js': ['src/js/excelsior/respCharts.js'],
-                    'js/excelsior/responsive-tables.min.js': ['src/js/excelsior/responsive-tables.js'],
-                    'js/site.min.js': ['src/js/site.js']
-                }
-            }
-        },
 
         less: {
             development: {
                 options: {
-                    dumpLineNumbers: "all"
-                },
-                files: {
-                    "src/css/excelsior.css": "src/less/excelsior.less",
-                    "src/css/off-canvas.css": "src/less/off-canvas.less"
-                }
-            },
-            production: {
-                options: {
                     yuicompress: true
                 },
                 files: {
-                    "css/excelsior.min.css": "src/less/excelsior.less",
-                    "css/off-canvas.min.css": "src/less/off-canvas.less",
-                    "css/site.min.css": "src/css/site.css"
+                    "css/excelsior.css": "less/excelsior.less",
+                    "css/off-canvas.css": "less/off-canvas.less"
                 }
             }
+            // ,
+            // production: {
+            //     options: {
+            //         yuicompress: true
+            //     },
+            //     files: {
+            //         "css/excelsior.min.css": "less/excelsior.less",
+            //         "css/off-canvas.min.css": "less/off-canvas.less",
+            //         "css/site.min.css": "css/site.css"
+            //     }
+            // }
         },
 
         jshint: {
@@ -55,16 +40,34 @@ module.exports = function(grunt) {
                 }
             }
         }
+
+        //,
+        // uglify: {
+        //     options: {
+        //         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+        //         mangle: false, // Don't change variable and function names
+        //         report: 'min'  // Print size savings to the command line
+        //     },
+        //     my_target: {
+        //         files: {
+        //             'js/excelsior/excelsior.min.js': ['js/excelsior/excelsior.js'],
+        //             'js/excelsior/off-canvas.min.js': ['js/excelsior/off-canvas.js'],
+        //             'js/excelsior/respCharts.min.js': ['js/excelsior/respCharts.js'],
+        //             'js/excelsior/responsive-tables.min.js': ['js/excelsior/responsive-tables.js'],
+        //             'js/site.min.js': ['js/site.js']
+        //         }
+        //     }
+        // }
     });
 
     // Load the plugins
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    // grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Default tasks
-    grunt.registerTask('default', ['uglify']);
     grunt.registerTask('default', ['less']);
     grunt.registerTask('default', ['jshint']);
+    // grunt.registerTask('default', ['uglify']);
 
 };
