30929f0c5550b6c5e5967714719752523f9dcd41
Craig Patik <cpatik@gmail.com>
2013-04-28 13:11:22 -0400

    Removed some temp file creations

--- Gruntfile.js
+++ Gruntfile.js
@@ -121,32 +121,22 @@ module.exports = function(grunt) {
                 ]
             },
             dist: {
-              files: {
-                // Copy temporarily so we can write to the same file name
-                'excelsior/css/temp-excelsior.css': ['excelsior/css/excelsior.css'],
-                // Unminified files
-                'excelsior/css/excelsior.css': ['excelsior/scss/foundation/normalize.css',
-                                                'excelsior/scss/foundation/foundation.css',
-                                                'excelsior/css/temp-excelsior.css'],
-                // Minified files
-                'excelsior/css/excelsior.min.css': ['excelsior/css/temp-normalize.min.css',
-                                                    'excelsior/css/temp-foundation.min.css',
-                                                    'excelsior/css/temp-excelsior.min.css']
+                files: {
+                    'excelsior/css/excelsior.css': ['excelsior/css/temp-excelsior.css']
+                }
+            },
+            preMinify: {
+                files: {
+                    'excelsior/css/temp-excelsior.css': ['excelsior/scss/foundation/normalize.css',
+                                                         'excelsior/scss/foundation/foundation.css',
+                                                         'excelsior/css/excelsior.css']
               }
             }
         },
         cssmin: {
-            normalize: {
-                src: 'excelsior/scss/foundation/normalize.css',
-                dest: 'excelsior/css/temp-normalize.min.css'
-            },
-            foundation_core: {
-                src: 'excelsior/scss/foundation/foundation.css',
-                dest: 'excelsior/css/temp-foundation.min.css'
-            },
             excelsior_core: {
-                src: 'excelsior/css/excelsior.css',
-                dest: 'excelsior/css/temp-excelsior.min.css'
+                src: 'excelsior/css/temp-excelsior.css',
+                dest: 'excelsior/css/excelsior.min.css'
             },
             excelsior_offCanvas: {
                 src: 'excelsior/css/off-canvas.css',
@@ -157,7 +147,7 @@ module.exports = function(grunt) {
             generatedFiles: {
                 src: ['excelsior/js/core/*.min.js', 'excelsior/css/*', 'excelsior/.sass-cache/', 'app/.sass-cache/', 'excelsior.zip']
             },
-            generatedFiles_dev: {
+            tempFiles: {
                 src: ['excelsior/css/temp-*.css']
             }
         }
@@ -171,14 +161,14 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    //grunt.loadNpmTasks('grunt-csscss'); //TODO: try to fix Ruby 2.0 error on windows
     grunt.loadNpmTasks('grunt-css');
+    //grunt.loadNpmTasks('grunt-csscss'); //TODO: try to fix Ruby 2.0 error on windows
 
     // Development
-    grunt.registerTask('dev', 'Development build', ['compass:excelsior_dev', 'compass:app_dev', 'jshint', 'cssmin', 'concat', 'clean:generatedFiles_dev']);
+    grunt.registerTask('dev', 'Development build', ['compass:excelsior_dev', 'compass:app_dev', 'jshint', 'concat:preMinify', 'cssmin', 'concat:dist', 'concat:addBanners', 'clean:tempFiles']);
 
     // Production
-    grunt.registerTask('prod', 'Production build', ['compass:clean', 'compass:excelsior_prod', 'compass:app_prod', 'uglify', 'cssmin', 'concat', 'clean:generatedFiles_dev']);
+    grunt.registerTask('prod', 'Production build', ['compass:clean', 'compass:excelsior_prod', 'compass:app_prod', 'uglify', 'concat:preMinify', 'cssmin', 'concat:dist', 'concat:addBanners', 'clean:tempFiles']);
 
     // Packager
     grunt.registerTask('package', 'Package up the project', ['compass:clean', 'compass:excelsior_prod', 'compass:excelsior_dev', 'uglify', 'cssmin', 'concat', 'clean', 'compress']);
