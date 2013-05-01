65f7f990b21294101bcddc405b0fb39b191a748f
Eric Steinborn <esteinborn@gmail.com>
2013-05-01 15:01:25 -0400

    abstract gov-links to top-links

--- excelsior/js/core/excelsior.js
+++ excelsior/js/core/excelsior.js
@@ -264,7 +264,7 @@ $(document).ready(function(){
   }
 
   // Standard Gov Banner display code
-  $('#gov-link-3').on(EWF.activateEventName, function(e) {
+  $('#top-link-3').on(EWF.activateEventName, function(e) {
     e.preventDefault();
     EWF.$body.addClass('active-top-bar-search');
   });
