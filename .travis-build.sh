2f6a4e53764f694bf5a4140d9c571bc5ae9435b7
Jeffrey Herb <herb.jeff@gmail.com>
2013-05-01 20:36:46 -0400

    New Travis build

---
+++ .travis-build.sh
@@ -0,0 +1,21 @@
+#Script File used by travis to mark when a build was preformed.
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis-CI"
+
+git clone --quiet --branch=${TRAVIS_BRANCH} https://${GH_TOKEN}@github.com/nys-its/excelsior.git  ${TRAVIS_BRANCH} > /dev/null
+
+cd ${TRAVIS_BRANCH}
+
+echo "Travis Build " >> .lastBuild
+echo date >> .lastBuild
+
+git add -f .
+git commit -m "TRAVIS BUILD $TRAVIS_BUILD_NUMBER"
+git push -fq origin ${TRAVIS_BRANCH} > /dev/null
+
+echo -e "Travis is done"
+
+fi
