030a17be5cdf3d8e18ca5152c1de3953702694ad
Jeffrey Herb <herb.jeff@gmail.com>
2013-05-03 11:40:20 -0400

    Upgrade to the most recent of foundation

--- excelsior/js/foundation/foundation.alerts.min.js
+++ excelsior/js/foundation/foundation.alerts.min.js
@@ -1,1 +1,50 @@
-(function($){"use strict";Foundation.libs.alerts={name:"alerts",version:"4.0.0",settings:{speed:300,callback:function(){}},init:function(scope,method,options){return this.scope=scope||this.scope,"object"==typeof method&&$.extend(!0,this.settings,method),"string"!=typeof method?(this.settings.init||this.events(),this.settings.init):this[method].call(this,options)},events:function(){var self=this;$(this.scope).on("click.fndtn.alerts","[data-alert] a.close",function(e){e.preventDefault(),$(this).closest("[data-alert]").fadeOut(self.speed,function(){$(this).remove(),self.settings.callback()})}),this.settings.init=!0},off:function(){$(this.scope).off(".fndtn.alerts")}}})(Foundation.zj,this,this.document);
+/*jslint unparam: true, browser: true, indent: 2 */
+
+;(function ($, window, document, undefined) {
+  'use strict';
+
+  Foundation.libs.alerts = {
+    name : 'alerts',
+
+    version : '4.0.0',
+
+    settings : {
+      speed: 300, // fade out speed
+      callback: function (){}
+    },
+
+    init : function (scope, method, options) {
+      this.scope = scope || this.scope;
+
+      if (typeof method === 'object') {
+        $.extend(true, this.settings, method);
+      }
+
+      if (typeof method != 'string') {
+        if (!this.settings.init) this.events();
+
+        return this.settings.init;
+      } else {
+        return this[method].call(this, options);
+      }
+    },
+
+    events : function () {
+      var self = this;
+
+      $(this.scope).on('click.fndtn.alerts', '[data-alert] a.close', function (e) {
+        e.preventDefault();
+        $(this).closest("[data-alert]").fadeOut(self.speed, function () {
+          $(this).remove();
+          self.settings.callback();
+        });
+      });
+
+      this.settings.init = true;
+    },
+
+    off : function () {
+      $(this.scope).off('.fndtn.alerts');
+    }
+  };
+}(Foundation.zj, this, this.document));
